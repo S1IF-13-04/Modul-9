1c775262927289b83eac7d9341aa8ec863aefa36
GamalielAlbert262 <ielsimanjuntak@gmail.com>
2025-11-16 15:16:05 +0700

    Add files via upload

---
+++ GAMALIEL ALBERT NATANAEL SIMAJUNTAK/tugas1.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+func main() {
+	var jumlahOrang int
+	fmt.Scan(&jumlahOrang)
+	jumlahMotor := jumlahOrang / 2
+	if jumlahOrang%2 != 0 {
+		jumlahMotor++
+	}
+	fmt.Println(jumlahMotor)
+}
